53c3c9e4faa0fb0ab0a7bbc1b76706ec2480c8af
pakizahsaqib <pakizahsaqib@gmail.com>
2024-08-17 20:38:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function decrement() {
+    count -= 1
+    if(count <= 0 )
+    {
+        countEl.textContent = 0;
+        count = 0
+    }
+    else
+    { 
+        countEl.textContent = count
+    };
+}
+
+function save() {
+    let countStr = count * 100;
+    saveEl.textContent =  "Total Price: $" + countStr
+   
+}
